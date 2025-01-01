5f73dd43ea3e1967a1df22fd7dee85e6736e7ff3
PRATEEK K <164144474+PRATEEKK9223@users.noreply.github.com>
2025-01-01 23:50:35 +0530

    Create Macro3.c

---
+++ Macro3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ printf("current date of system=%s\n",__DATE__);
+ printf("current time of system=%s\n",__TIME__);
+ printf("file name =%s\n",__FILE__);
+ printf("the number of line=%d\n",__LINE__);
+ printf("ANSI C standard =%d\n",__STDC__);
+ printf("version of the compiler=%d\n",__VERSION__);
+ 
+}
+
+Predefined macros
